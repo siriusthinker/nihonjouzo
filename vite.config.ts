1f0bd9f277229b387ee023501c273c0b728807db
Tristan Lisondra <tristan.lisondra@aikomi.co.jp>
2025-05-18 23:15:42 +0900

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,5 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nihonjouzo/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+  base: '/nihonjouzo/'
 })
